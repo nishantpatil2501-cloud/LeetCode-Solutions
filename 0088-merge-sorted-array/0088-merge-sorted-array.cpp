86b52be20bc292f7031168a6b255ab490d942a12
Nishant Patil <nishantpatil2501@gmail.com>
2026-07-28 16:47:22 +0530

    Time: 0 ms (100%), Space: 12.4 MB (21.39%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void merge(vector<int>& a, int m, vector<int>& b, int n) {
+        int idx=m+n-1;
+        int i=m-1;
+        int j=n-1;
+        while(i>=0&&j>=0){
+            if(a[i]>b[j]){
+                a[idx]=a[i];
+                i--;idx--;
+            }
+            else{
+                a[idx]=b[j];
+                j--;idx--;
+
+            }
+        }
+        while(j>=0){
+            a[idx]=b[j];
+            j--;idx--;
+        }
+
+        
+    }
+};
